d0cbc1f64d17720a32d57f62f38166079979b6a1
gusdev0258 <16326176760@edu.udesc.br>
2023-05-18 18:42:46 -0300

    feat: creating energyBill homepage

--- src/view/energyBill/EnergyBill.jsx
+++ src/view/energyBill/EnergyBill.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import DefaultInput from '../utils/form/DefaultInput';
-
+import React from "react";
+import DefaultInput from "../utils/form/DefaultInput";
+import { MagnifyingGlass, List, Flag } from "@phosphor-icons/react";
+import Header from "../utils/Header";
 
 const EnergyBill = () => {
-  const [search, setSearch] = React.useState('');
+  const [search, setSearch] = React.useState("");
 
-  function handleSearch({target}){
-  }
+  function handleSearch({ target }) {}
 
   return (
     <div>
-      <section className='default-form-container'>
-      <form className="form-container">
-      <DefaultInput className="searchInput default-form-input" id="search" type="search" value={search} setValue={setSearch} placeholder="Pesquisar faturas..."/>
-      </form>
+      <Header textContent="Minhas Faturas" />
+
+      <section className="default-form-container">
+        <form className="form-container">
+          <DefaultInput
+            className="search-input default-form-input"
+            id="search"
+            type="search"
+            value={search}
+            setValue={setSearch}
+            placeholder="Pesquisar faturas..."
+          />
+          <MagnifyingGlass size={16} className="search-icon" color="#482803" />
+        </form>
       </section>
       {search}
     </div>
-  )
-}
+  );
+};
 
-export default EnergyBill
+export default EnergyBill;
